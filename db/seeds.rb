0985a79b0049e44b38a0dc0fbae566193ecdef3a
Elbay Malikmammadov <eleo.666@gmail.com>
2022-10-08 22:48:21 +0200

    seed a representative flat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Tight & Small flat in Berlin',
+  address: 'Berlin Mitte',
+  description: 'This is very disturbing but you cannot find any other place to live - so take it or leave it',
+  price_per_night: 100,
+  number_of_guests: 1
+)
