b6c9896a67dab4889d7bf93f5c79427e3de71d3e
Elbay Malikmammadov <eleo.666@gmail.com>
2022-10-08 22:45:48 +0200

    update flats database

--- db/migrate/20221008204221_create_flats.rb
+++ db/migrate/20221008204221_create_flats.rb
@@ -1,6 +1,12 @@
 class CreateFlats < ActiveRecord::Migration[7.0]
   def change
     create_table :flats do |t|
+      t.string :name
+      t.string :address
+      t.string :picture_url
+      t.text :description
+      t.integer :price_per_night
+      t.integer :number_of_guests
 
       t.timestamps
     end
